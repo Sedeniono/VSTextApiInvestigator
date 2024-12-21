52e25211f7c3180e6f7c099e94117e6ab288b770
Sedenion <39583823+Sedeniono@users.noreply.github.com>
2024-12-21 18:04:02 +0100

    Inital commit: No functionality yet.

---
+++ VSTextApiInvestigator/VSTextApiInvestigatorPackage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSTextApiInvestigator
+{
+  [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+  [Guid(VSTextApiInvestigatorPackage.PackageGuidString)]
+  public sealed class VSTextApiInvestigatorPackage : AsyncPackage
+  {
+    public const string PackageGuidString = "120da8b9-77a4-48f8-b379-87d428a667ff";
+
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+    {
+      // When initialized asynchronously, the current thread may be a background thread at this point.
+      // Do any initialization that requires the UI thread after switching to the UI thread.
+      await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+    }
+  }
+}
